09198215c4d8dd0870f9190be99b9f969f95ed8c
bitnami <fung933@gmail.com>
2014-07-29 08:01:08 +0000

    homework first rb changes completed

--- erb_example.rb
+++ erb_example.rb
@@ -3,3 +3,8 @@ require 'erb'
 x = 42
 template = ERB.new "The value of x is: <%= x %>"
 puts template.result(binding)
+
+y = [1, 2, 3, 4]
+str = "valid"
+item = ERB.new "The value of y is <%= str %>"
+puts item.result(binding)
